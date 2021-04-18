df6080119cb74b094fe06afa4880863b5d8a96a3
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-18 13:59:16 +0600

    basic setup added

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
+// dotenv.config('./backend/config/.env');
+dotenv.config({path : './backend/config/.env'});
+
+
+
+const app = express();
+
+
+connectDB();
+
+app.use(express.json());
+
+app.get('/',(req,res)=> {
+    res.status('Api is running....')
+})
+
+
+
+app.listen(process.env.PORT , console.log(`Server running in  on port ${process.env.PORT}`.yellow.bold));
