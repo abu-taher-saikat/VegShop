9428351160f110c2439e7c62768d47cc129f0e6a
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-04-18 23:28:31 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const connectDB = require('./config/db');
-const colors = require('colors');
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // dotenv.config('./backend/config/.env');
-dotenv.config({path : './backend/config/.env'});
-
-
+dotenv.config({ path: "./backend/config/.env" });
 
 const app = express();
-
 
 connectDB();
 
 app.use(express.json());
 
-app.get('/',(req,res)=> {
-    res.status('Api is running....')
-})
+app.get("/", (req, res) => {
+  res.send("Api is running....");
+});
 
-
-
-app.listen(process.env.PORT , console.log(`Server running in  on port ${process.env.PORT}`.yellow.bold));
+app.listen(
+  process.env.PORT,
+  console.log(`Server running in  on port ${process.env.PORT}`.yellow.bold)
+);
