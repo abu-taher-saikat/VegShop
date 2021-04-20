6e62f3c2e77d214f80ae102a1b65b770d85f040d
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-20 11:33:43 +0600

    basic auth added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -48,4 +48,22 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 });
 
-console.log(typeof generateToken);
+
+
+// @desc Get User profile
+// @route GET /api/v1/auth/profile
+// @access Private
+exports.profile = asyncHandler(async (req,res) => {
+  const user = await User.findById(req.user._id);
+
+  if(user){
+      res.json({
+          _id : user._id,
+          name : user.name,
+          email : user.email,
+      })
+  }else{
+      res.status(404)
+      throw new Error('User not found')
+  }
+})
