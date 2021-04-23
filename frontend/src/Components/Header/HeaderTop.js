6a58acbf31c8ab687a5614675f18cbaa80508313
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-24 00:05:01 +0600

    Header Top Added

--- frontend/src/Components/Header/HeaderTop.js
+++ frontend/src/Components/Header/HeaderTop.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
+import { AiFillTwitterCircle, AiOutlineInstagram } from 'react-icons/ai';
+import { FaFacebookSquare, FaPinterestP } from 'react-icons/fa';
 import './Header.css';
 
 const HeaderTop = () => {
@@ -8,23 +10,31 @@ const HeaderTop = () => {
     return (
         <div className="headerTop">
             <div className="container">
-                <div classnames="left">
-                    <div >Mon - Fri: 9:00am - 19:00pm</div>
-                    <span> | </span>
+                <div className="left">
+                    <div className="" >Mon - Fri: 9:00am - 19:00pm</div>
+                    <div className="px-2"> | </div>
                     <div >support@gmail.com</div>
                 </div>
-                <div classnames='right'>
-                    <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                USD
-                            </Dropdown.Toggle>
+                <div className='right'>
+                    <div>
+                        <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    USD
+                                </Dropdown.Toggle>
 
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                            </Dropdown.Menu>
-                    </Dropdown>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">USD</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">BDT </Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">NRI</Dropdown.Item>
+                                </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className="">
+                        <FaFacebookSquare className="social-link mr-1" />
+                        <AiFillTwitterCircle className="social-link mx-1" />
+                        <AiOutlineInstagram className="social-link mx-1" />
+                        <FaPinterestP className="social-link ml-1" />
+                    </div>
                 </div>
             </div>
         </div>
