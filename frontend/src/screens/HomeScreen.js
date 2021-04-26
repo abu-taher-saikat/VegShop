cb4bcb39aba2f69f2163ebff573a72dcf94a045d
saikat1095 <mdabuthaersaikat@gmail.com>
2021-04-27 01:43:46 +0600

    basic error css fixed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,25 @@
 import React from "react";
 import BestSeller from "../Components/BestSeller";
+import Footer from '../Components/Footer/Footer';
 import ImageOffers from "../Components/ImageOffers";
-import NewArraivle from "../Components/NewArraivle";
+import LatestBlogPost from '../Components/LatestBlogPost';
+import LogoList from "../Components/LogoList";
+// import NewArraivle from "../Components/NewArraivle";
 import WebsiteFeature from "./../Components/WebsiteFeature";
 
 const HomeScreen = () => {
   return (
-    <div className="container">
-      <WebsiteFeature></WebsiteFeature>
-      <BestSeller></BestSeller>
-      <ImageOffers></ImageOffers>
-      <NewArraivle></NewArraivle>
-    </div>
+      <>
+        <div className="container">
+        <WebsiteFeature></WebsiteFeature>
+        <BestSeller></BestSeller>
+        <ImageOffers></ImageOffers>
+        <LogoList></LogoList>
+        <LatestBlogPost></LatestBlogPost>
+        {/* <NewArraivle></NewArraivle> */}
+        </div>
+        <Footer></Footer>
+    </>
   );
 };
 
