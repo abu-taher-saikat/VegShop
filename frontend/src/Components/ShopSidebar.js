280f977c489898d98f6478cc33e02ff1d232d979
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-05-02 11:24:32 +0600

    input ranger changed

--- frontend/src/Components/ShopSidebar.js
+++ frontend/src/Components/ShopSidebar.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { useState } from "react";
-import InputRange from "react-input-range";
 
 function ShopSidebar() {
-  const [InputValue, setValue] = useState({ value: { min: 2, max: 10 } });
   return (
     <div>
       <b>Categories</b>
@@ -15,12 +13,7 @@ function ShopSidebar() {
       <button className="btn btn-cus"> Sale off</button>
       <hr />
       <b>Filter by price</b>
-      <InputRange
-        maxValue={20}
-        minValue={0}
-        value={InputValue}
-        onChange={(value) => setValue({ value })}
-      />
+
       <button className="btn btn-primary">Filter</button>
       <h6>Best sellers</h6>
     </div>
